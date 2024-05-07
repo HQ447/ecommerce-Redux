bf0ba80e7647f307982ed6d862d59dc5b60109c2
Hammad Ahmad <hamadqur447@gmail.com>
2024-05-07 21:31:21 +0500

    authentication

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,13 +1,20 @@
 import "./Navbar.css";
 import logo3 from "../Assets/logo3.png";
+import { useAuth0 } from "@auth0/auth0-react";
 import { NavLink } from "react-router-dom";
 import { HiMiniShoppingCart } from "react-icons/hi2";
 import { TiThMenu } from "react-icons/ti";
 import { useState } from "react";
 import { useSelector } from "react-redux";
+import { FiLogOut } from "react-icons/fi";
+import { FaUserCircle } from "react-icons/fa";
 
 function Navbar({ setshowAccount }) {
   const cart = useSelector((state) => state.cart.cart);
+
+  const { user, isAuthenticated } = useAuth0();
+  const { loginWithRedirect } = useAuth0();
+  const { logout } = useAuth0();
 
   const [showHiddenMenu, setshowHiddenMenu] = useState(false);
 
@@ -26,14 +33,7 @@ function Navbar({ setshowAccount }) {
             <li onClick={() => setshowHiddenMenu(false)}>Product</li>
           </NavLink>
           <NavLink>
-            <li
-              onClick={() => {
-                setshowAccount(true);
-                setshowHiddenMenu(false);
-              }}
-            >
-              Contact
-            </li>
+            <li onClick={() => loginWithRedirect()}>Contact</li>
           </NavLink>
         </ul>
       </div>
@@ -50,14 +50,25 @@ function Navbar({ setshowAccount }) {
         <NavLink to={"/Product"}>
           <li>Product</li>
         </NavLink>
-        <NavLink>
-          <li onClick={() => setshowAccount(true)}>Contact</li>
+        <NavLink className="bg-blue-500 py-2 px-2 rounded-lg text-white">
+          <li onClick={() => loginWithRedirect()}>Contact</li>
         </NavLink>
       </ul>
       <div className="nav-login-cart flex items-center gap-3 ">
+        <p className="-xsm:text-sm">{isAuthenticated ? user.name : "Guest"}</p>
+        {isAuthenticated ? (
+          <FiLogOut
+            className="text-3xl -xsm:text-2xl"
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+          />
+        ) : (
+          <FaUserCircle className="text-3xl -xsm:text-2xl" />
+        )}
         <div className="relative">
           <NavLink to={"/cart"}>
-            <HiMiniShoppingCart className="text-3xl -xsm:text-3xl transition-all hover:scale-105 " />
+            <HiMiniShoppingCart className="text-3xl -xsm:text-2xl transition-all hover:scale-105 " />
             <div className="nav-cart text-xs count w-4 absolute -top-2 -right-2 h-4 flex justify-center items-center bg-red-600 text-white rounded-full">
               {cart.length}
             </div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,25 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store";
+import { Auth0Provider } from "@auth0/auth0-react";
 import { BrowserRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 
 // Render the application
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <Auth0Provider
+      domain="dev-t5g8kap2mn3bp8u2.us.auth0.com"
+      clientId="fHCWf0QRhizVhFu66Ln7RkKSdrKbHtKW"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </Auth0Provider>
   </React.StrictMode>
 );
